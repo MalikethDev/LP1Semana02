06e1e5e3802e1b04f9b8457469773af37139797e
Rodrigo <rodrigogomes091997@gmail.com>
2025-02-27 16:25:35 +0000

    Shift left by 3

--- VariousOperators/Program.cs
+++ VariousOperators/Program.cs
@@ -10,6 +10,11 @@ namespace VariousOperators
             Console.WriteLine("Please enter a non negative integer number: ");
             // Store the input in byte
             byte n = byte.Parse(Console.ReadLine());
+
+            // Print the division of n by 2
+            Console.WriteLine($"The result of the division operation is: {n / 2}");
+            // Shift left by 3 bits and print the result
+            Console.WriteLine($"The result of the shift left operation is: {n << 3}");
         }
     }
 }
