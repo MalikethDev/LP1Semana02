d4eb563682cdf9265fa2deb35e835625fc63e289
Rodrigo <rodrigogomes091997@gmail.com>
2025-02-27 15:13:45 +0000

    Show the result of the decrement operation

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -10,6 +10,11 @@ namespace SmoothOperator
             Console.WriteLine("Please enter an integer number: ");
             // Store the input in sbyte
             sbyte n = sbyte.Parse(Console.ReadLine());
+
+            // Result of the n decrement operation
+            sbyte result = --n;
+            // Print the result
+            Console.WriteLine($"The result of the decrement operation is: {result}");
         }
     }
 }
