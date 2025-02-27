2b59e2bc31e3b5bc99e7fbe874ff9ee00af25585
Rodrigo <rodrigogomes091997@gmail.com>
2025-02-27 17:20:02 +0000

    Cilindro

---
+++ Cilindro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Ask the user for the height "a" of the cylinder
+            Console.WriteLine("Please enter the height of the cylinder: ");
+            // Store the input in double
+            double a = double.Parse(Console.ReadLine());
+            // Ask the user for the radius "r" of the cylinder
+            Console.WriteLine("Please enter the radius of the cylinder: ");
+            // Store the input in double
+            double r = double.Parse(Console.ReadLine());
+            // Calculate the volume "V" of the cylinder
+            double V = Math.PI * Math.Pow(r, 2) * a;
+            // Calculate the surface area Sa of the cylinder
+            double Sa = 2 * Math.PI * r * (r + a);
+        }
+    }
+}
