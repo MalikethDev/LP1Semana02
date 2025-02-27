f90b852d95d6338d06ec78a61d4eb01aef4fcb5d
Rodrigo <rodrigogomes091997@gmail.com>
2025-02-27 15:51:03 +0000

    Ask for a negative int number and store it in bytein VariousOperators

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Ask the user for an non negative int number
+            Console.WriteLine("Please enter a non negative integer number: ");
+            // Store the input in byte
+            byte n = byte.Parse(Console.ReadLine());
+        }
+    }
+}
