083ebf103f4ceafc57785a999cde51e0bda1db0b
Rodrigo <rodrigogomes091997@gmail.com>
2025-02-27 14:53:36 +0000

    Ask the user for an int number in SmoothOperator

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Ask the user for an int number
+            Console.WriteLine("Please enter an integer number: ");
+            // Read the user's input
+            int number = int.Parse(Console.ReadLine());
+        }
+    }
+}
