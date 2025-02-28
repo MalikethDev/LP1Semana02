390c66afc23ee06fb8d546ac491122faa41d79d5
Rodrigo <rodrigogomes091997@gmail.com>
2025-02-28 01:19:33 +0000

    Added answers to chatbot

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -9,6 +9,34 @@ namespace ChatGPTOne
             // Ask the user to ask a question adn store it in a variable
             Console.WriteLine("Please ask a question: ");
             string question = Console.ReadLine();
+
+            // Variable to store the answer 
+            // Necessary
+            string answer;
+
+            // Recognizing the question and providing an answer
+            switch (question)
+            {
+                case "How are you?":
+                    answer = "I'm good, thank you!";
+                    break;
+
+                case "What's your name?":
+                    answer = "I'm ChatGPToOne!";
+                    break;
+
+                case "What's your mission?":
+                    answer = "To help you with your questions!";
+                    break;
+                // Default case if the question is not recognized
+                // Necessary
+                default:
+                    answer = "I'm not sure how to answer that!";
+                    break;
+            }
+
+            // Print the answer
+            Console.WriteLine(answer);
         }
     }
 }
