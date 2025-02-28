9f6e927e24f15ceb4062ba0189a40fc88011af41
Rodrigo <rodrigogomes091997@gmail.com>
2025-02-28 01:48:59 +0000

    Added while loop to keep it running

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -5,47 +5,51 @@ namespace ChatGPTOne
     public class Program
     {
         private static void Main(string[] args)
-        {   
-            // Ask the user to ask a question adn store it in a variable
-            Console.WriteLine("Please ask a question: ");
-            string question = Console.ReadLine();
-
-            // Variable to store the answer 
-            // Necessary
-            string answer;
-
-            // Recognizing the question and providing an answer
-            switch (question)
+        {  
+            // Infinite loop to keep the program running
+            while (true)
             {
-                case "How are you?":
-                    answer = "I'm good, thank you!";
-                    break;
+                // Ask the user to ask a question adn store it in a variable
+                Console.WriteLine("Please ask a question: ");
+                string question = Console.ReadLine();
 
-                case "What's your name?":
-                    answer = "I'm ChatGPToOne!";
-                    break;
-
-                case "What's your mission?":
-                    answer = "To help you with your questions!";
-                    break;
-
-                case "Are you more intelligent than me?":
-                    answer = "I'm just a program, so I don't have intelligence!";
-                    break;
-
-                // If the user enters EXIT the program will end
-                case "EXIT":
-                    return;
-                    
-                // Default case if the question is not recognized
+                // Variable to store the answer 
                 // Necessary
-                default:
-                    answer = "I'm not sure how to answer that!";
-                    break;
-            }
+                string answer;
 
-            // Print the answer
-            Console.WriteLine(answer);
+                // Recognizing the question and providing an answer
+                switch (question)
+                {
+                    case "How are you?":
+                        answer = "I'm good, thank you!";
+                        break;
+
+                    case "What's your name?":
+                        answer = "I'm ChatGPToOne!";
+                        break;
+
+                    case "What's your mission?":
+                        answer = "To help you with your questions!";
+                        break;
+
+                    case "Are you more intelligent than me?":
+                        answer = "I'm just a program, so I don't have intelligence!";
+                        break;
+
+                    // If the user enters EXIT the program will end
+                    case "EXIT":
+                        return;
+
+                    // Default case if the question is not recognized
+                    // Necessary
+                    default:
+                        answer = "I'm not sure how to answer that!";
+                        break;
+                }
+
+                // Print the answer
+                Console.WriteLine(answer);
+            } 
         }
     }
 }
